30d60d32e63ec506e7ef18fa02f3a7bbcf6e667b
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-03-31 13:41:35 -0700

    feat(PBI-ADM): Add implementation of Responsible

---
+++ src/Application/Validators/ResponsibleValidator.cs
@@ -0,0 +1,13 @@
+﻿using Enflorarte.CRM.Domain.Entities;
+
+namespace Enflorarte.CRM.Application.Validators;
+
+public class ResponsibleValidator : AbstractValidator<Responsible>
+{
+    public ResponsibleValidator()
+    {
+        RuleFor(responsible => responsible.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
---
+++ src/Infrastructure/Data/Dommon/Models/ResponsibleDAO.cs
@@ -0,0 +1,9 @@
+﻿using Enflorarte.CRM.Application.Common.Interfaces.DAOs;
+using Enflorarte.CRM.Domain.Entities;
+
+namespace Enflorarte.CRM.Infrastructure.Data.Dommon.Models;
+
+public class ResponsibleDAO : BaseDAO<Responsible>, IResponsibleDAO
+{
+    public ResponsibleDAO(ApplicationDbContext context) : base(context) { }
+}
