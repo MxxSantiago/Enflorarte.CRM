bff9c9fb7599e8e420f415a6bb675c4ff76173aa
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-03-31 13:34:56 -0700

    feat(PBI-ADM): Add implementation of Flower

---
+++ src/Web/Controllers/FlowerController.cs
@@ -0,0 +1,11 @@
+﻿using Enflorarte.CRM.Application.Services;
+using Enflorarte.CRM.Domain.Entities;
+
+namespace Enflorarte.CRM.Web.Controllers;
+
+public class FlowerController : BaseController<Flower, FlowerService>
+{
+    public FlowerController(FlowerService service) : base(service)
+    {
+    }
+}
