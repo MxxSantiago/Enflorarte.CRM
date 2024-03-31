3cf346f4d9060934bc88d5eb16bcd51d0af8853c
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-03-31 13:36:04 -0700

    feat(PBI-ADM): Add implementation of FlowerVariant

---
+++ src/Infrastructure/Data/Dommon/Models/FlowerVariantDAO.cs
@@ -0,0 +1,13 @@
+﻿using Enflorarte.CRM.Application.Common.Interfaces.DAOs;
+using Enflorarte.CRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enflorarte.CRM.Infrastructure.Data.Dommon.Models;
+
+public class FlowerVariantDAO : BaseDAO<FlowerVariant>, IFlowerVariantDAO
+{
+    public FlowerVariantDAO(ApplicationDbContext context) : base(context) { }
+
+    public async Task<List<FlowerVariant>> GetByFlower(int flowerId) =>
+        await _context.FlowerVariant.Where(x => x.FlowerId == flowerId).ToListAsync();
+}
