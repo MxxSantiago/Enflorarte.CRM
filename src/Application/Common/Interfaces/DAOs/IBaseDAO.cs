c4623dd4d1fb02e3fa209e614b86bb37d731a347
Max Santiago <mxxsant@hotmail.com>
2024-03-30 23:47:17 -0700

    feat(PBI-GEN-04): Add base DAO

---
+++ src/Application/Common/Interfaces/DAOs/IBaseDAO.cs
@@ -0,0 +1,11 @@
+﻿namespace Enflorarte.CRM.Application.Common.Interfaces.DAOs;
+
+public interface IBaseDAO<TEntity> where TEntity : class
+{
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    Task<IEnumerable<TEntity>?> GetAllAsync();
+    Task<TEntity?> GetAsync(int id);
+    Task Delete(int id);
+    Task SaveChangesAsync();
+}
---
+++ src/Infrastructure/Data/Dommon/Models/BaseDAO.cs
@@ -0,0 +1,30 @@
+﻿using Enflorarte.CRM.Application.Common.Interfaces.DAOs;
+using Enflorarte.CRM.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enflorarte.CRM.Infrastructure.Data.Dommon.Models;
+
+public abstract class BaseDAO<TEntity>(ApplicationDbContext context) : IBaseDAO<TEntity>
+    where TEntity : BaseEntity
+{
+    protected readonly ApplicationDbContext _context = context;
+
+    public virtual void Add(TEntity entity) => _context.Set<TEntity>().Add(entity);
+
+    public virtual void Update(TEntity entity) => _context.Set<TEntity>().Update(entity);
+
+    public virtual async Task<IEnumerable<TEntity>?> GetAllAsync() => await _context.Set<TEntity>().ToListAsync();
+
+    public virtual async Task<TEntity?> GetAsync(int id) => await _context.Set<TEntity>().FindAsync(id);
+
+    public async Task Delete(int id)
+    {
+        var entity = await GetAsync(id);
+        if (entity != null)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+    }
+    
+    public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+}
