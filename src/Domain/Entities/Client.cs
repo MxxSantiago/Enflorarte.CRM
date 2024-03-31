774c163b6fb6bbf41fed3074b481fab22f2f825d
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-03-31 01:57:31 -0700

    feat(PBI-ADM): Add Client entity

---
+++ src/Domain/Entities/Client.cs
@@ -0,0 +1,10 @@
+namespace Enflorarte.CRM.Domain.Entities;
+
+public class Client : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string PreferredAddress { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public int PreferredCommunicationTypeId { get; set; }
+    public CommunicationType PreferredCommunicationType { get; set; } = null!;
+}
