38459aa91bbbe7f463adf7611c9303f226a67a95
Max Santiago <mxxsant@hotmail.com>
2024-03-30 23:59:08 -0700

    feat(PBI-GEN-05): Add base service

---
+++ src/Application/Common/Models/BaseService.cs
@@ -0,0 +1,45 @@
+﻿using Enflorarte.CRM.Application.Common.Interfaces.DAOs;
+using Enflorarte.CRM.Domain.Common;
+
+namespace Enflorarte.CRM.Application.Common.Models;
+
+public abstract class BaseService<TEntity>(IBaseDAO<TEntity> dao, IValidator<TEntity> validator)
+    where TEntity : BaseEntity
+{
+    public async Task<IList<TEntity>> GetAllAsync()
+    {
+        return (await dao.GetAllAsync() ?? new List<TEntity>()).ToList();
+    }
+
+    public async Task<TEntity?> GetAsync(int id) => await dao.GetAsync(id);
+    
+    public async Task AddAsync(TEntity entity)
+    {
+        await ValidateAsync(entity);
+        dao.Add(entity);
+        await dao.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        await ValidateAsync(entity);
+        dao.Update(entity);
+        await dao.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await dao.Delete(id);
+        await dao.SaveChangesAsync();
+    }
+    
+    private async Task ValidateAsync(TEntity entity)
+    {
+        var validationResult = await validator.ValidateAsync(entity);
+
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+    }
+}
