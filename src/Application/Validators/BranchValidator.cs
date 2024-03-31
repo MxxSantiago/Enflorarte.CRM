038374aec31f6e78b233331db595e403ce31774e
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-03-31 13:31:14 -0700

    feat(PBI-ADM): Add implementation of branch

---
+++ src/Application/Validators/BranchValidator.cs
@@ -0,0 +1,13 @@
+﻿using Enflorarte.CRM.Domain.Entities;
+
+namespace Enflorarte.CRM.Application.Validators;
+
+public class BranchValidator : AbstractValidator<Branch>
+{
+    public BranchValidator()
+    {
+        RuleFor(branch => branch.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
---
+++ src/Web/Controllers/BranchController.cs
@@ -0,0 +1,11 @@
+﻿using Enflorarte.CRM.Application.Services;
+using Enflorarte.CRM.Domain.Entities;
+
+namespace Enflorarte.CRM.Web.Controllers;
+
+public class BranchController : BaseController<Branch, BranchService>
+{
+    public BranchController(BranchService service) : base(service)
+    {
+    }
+}
