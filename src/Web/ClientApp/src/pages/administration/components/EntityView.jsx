9ed40122aa442e9a3ff07334f4045fb51280beb7
JosueCV02 <josuecarreon2003@outlook.com>
2024-05-07 02:06:59 -0700

    feat(PBI-GEN)
    Centrar vista de EntityView

--- src/Web/ClientApp/src/pages/administration/components/EntityView.jsx
+++ src/Web/ClientApp/src/pages/administration/components/EntityView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import CreateEntity from "./CreateEntity.jsx";
 import { CommunicationType } from "../../../web-api-client.ts";
 import EntitiesTable from "./entitiesTable/EntitiesTable.jsx";
@@ -7,29 +7,29 @@ import { removeReferenceIdProperties } from "../../../core/helpers/web-api-clien
 
 const EntityView = ({ title, entityName, entity, fatherEntityName }) => {
   const [lastUpdated, setLastUpdated] = useState();
-
   const refreshView = () => setLastUpdated(new Date());
-
   useEffect(() => refreshView(), [entityName]);
 
   return (
-    <Flex direction="column" gap={5} px={{ base: 4, md: 6 }} py={6}>
-      <CreateEntity
-        entityName={entityName}
-        title={title}
-        entity={removeReferenceIdProperties(entity)}
-        refreshView={refreshView}
-        fatherEntity={new CommunicationType().toJSON()}
-        fatherEntityName="communicationType"
-      />
-      <EntitiesTable
-        entity={entity}
-        entityName={entityName}
-        fatherEntityName={fatherEntityName}
-        refreshView={refreshView}
-        lastUpdated={lastUpdated}
-      />
-    </Flex>
+    <Box maxW={{ base: "100%", md: "800px" }} w="100%" mx="auto" px={4}>
+      <Flex direction="column" gap={5} px={{ base: 0, md: 6 }} py={6}>
+        <CreateEntity
+          entityName={entityName}
+          title={title}
+          entity={removeReferenceIdProperties(entity)}
+          refreshView={refreshView}
+          fatherEntity={new CommunicationType().toJSON()}
+          fatherEntityName="communicationType"
+        />
+        <EntitiesTable
+          entity={entity}
+          entityName={entityName}
+          fatherEntityName={fatherEntityName}
+          refreshView={refreshView}
+          lastUpdated={lastUpdated}
+        />
+      </Flex>
+    </Box>
   );
 };
 
