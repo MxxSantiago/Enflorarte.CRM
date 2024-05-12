798c358e2d2ec022ee037a67e2e7ff5083152f3e
Max Santiago <mxxsant@hotmail.com>
2024-05-11 20:24:04 -0700

    feat(PBI-GEN-16): Add cache to HTTP requests

--- src/Web/ClientApp/src/core/helpers/web-api-client.helper.ts
+++ src/Web/ClientApp/src/core/helpers/web-api-client.helper.ts
@@ -1,62 +1,117 @@
-// @ts-ignore
 import {
-    ArrangementClient,
-    ArrangementTypeClient,
-    BranchClient,
-    ClientClient,
-    CommunicationTypeClient,
-    DeliveryTypeClient,
-    FlowerClient,
-    FlowerVariantClient,
-    ResponsibleClient,
-    WrapperClient,
-    WrapperVariantClient,
+  ArrangementClient,
+  ArrangementTypeClient,
+  BranchClient,
+  ClientClient,
+  CommunicationTypeClient,
+  DeliveryTypeClient,
+  FlowerClient,
+  FlowerVariantClient,
+  ResponsibleClient,
+  WrapperClient,
+  WrapperVariantClient,
 } from "../../web-api-client.ts";
-
-function selectClient(entityName: string) {
-    switch (entityName.trim()) {
-        case "arrangement":
-            return new ArrangementClient();
-        case "arrangementType":
-            return new ArrangementTypeClient();
-        case "branch":
-            return new BranchClient();
-        case "client":
-            return new ClientClient();
-        case "communicationType":
-            return new CommunicationTypeClient();
-        case "deliveryType":
-            return new DeliveryTypeClient();
-        case "flower":
-            return new FlowerClient();
-        case "flowerVariant":
-            return new FlowerVariantClient();
-        case "responsible":
-            return new ResponsibleClient();
-        case "wrapper":
-            return new WrapperClient();
-        case "wrapperVariant":
-            return new WrapperVariantClient();
-        default:
-            throw new Error("Invalid entity name");
-  }
-}
 
 type MethodNames = "Post" | "Get" | "GetAll" | "Put" | "Delete";
 
-export async function getAllEntities(
-  entityName: string,
-  removeReferences: boolean = false
-) {
-  const result = await executeCrudMethod(entityName, "GetAll");
+class ApiClient {
+  private clients = {
+    arrangement: new ArrangementClient(),
+    arrangementType: new ArrangementTypeClient(),
+    branch: new BranchClient(),
+    client: new ClientClient(),
+    communicationType: new CommunicationTypeClient(),
+    deliveryType: new DeliveryTypeClient(),
+    flower: new FlowerClient(),
+    flowerVariant: new FlowerVariantClient(),
+    responsible: new ResponsibleClient(),
+    wrapper: new WrapperClient(),
+    wrapperVariant: new WrapperVariantClient(),
+  };
 
-  if (removeReferences) {
-    result.forEach((entity) => removeReferenceIdProperties(entity));
+  private clientCache: { [key: string]: any } = {};
+
+  private getCachedResult(cacheKey: string, refresh: boolean) {
+    if (!refresh && this.clientCache[cacheKey]) {
+      return this.clientCache[cacheKey];
+    }
   }
 
-  return result;
+  public async getAllEntities(
+    entityName: string,
+    removeReferences: boolean = false,
+    refresh: boolean = false
+  ) {
+    const cacheKey = entityName;
+    const cachedResult = this.getCachedResult(cacheKey, refresh);
+
+    if (cachedResult) {
+      return cachedResult;
+    }
+
+    const result = await this.executeHttpMethod(entityName, "GetAll");
+    this.clientCache[cacheKey] = result;
+    if (removeReferences) {
+      result.forEach((entity: any) => removeReferenceIdProperties(entity));
+    }
+    return result;
+  }
+
+  public async getEntity(
+    entityName: string,
+    id: number,
+    refresh: boolean = false
+  ) {
+    const cacheKey = `${entityName}_${id}`;
+    const cachedResult = this.getCachedResult(cacheKey, refresh);
+
+    if (cachedResult) {
+      return cachedResult;
+    }
+
+    const result = await this.executeHttpMethod(entityName, "Get", id);
+    this.clientCache[cacheKey] = result;
+
+    return result;
+  }
+
+  public deleteEntity = (entityName: string, id: number) =>
+    this.executeHttpMethod(entityName, "Delete", id);
+
+  public updateEntity = (entityName: string, args: any) =>
+    this.executeHttpMethod(entityName, "Put", args);
+
+  public createEntity = (entityName: string, args: any) =>
+    this.executeHttpMethod(entityName, "Post", args);
+
+  private async executeHttpMethod(
+    entityName: string,
+    methodName: MethodNames,
+    ...args: any[]
+  ) {
+    if (!this.clients[entityName]) {
+      throw new Error("Invalid entity name");
+    }
+
+    const client = this.clients[entityName];
+    const method = client[`${entityName}_${methodName}`];
+
+    if (typeof method === "function") {
+      const boundMethod = method.bind(client);
+      return methodName === "Put"
+        ? await boundMethod(args[0].id, ...args)
+        : await boundMethod(...args);
+    } else {
+      throw new Error(
+        `Method ${entityName}_${methodName} does not exist on ${entityName}Client`
+      );
+    }
+  }
 }
 
+export const apiClient = new ApiClient();
+
+// Utils
 export function removeReferenceIdProperties(payload: any) {
   const idProperties = Object.keys(payload)
     .filter((key) => key.indexOf("Id") !== -1 && key !== "id")
@@ -85,7 +140,7 @@ export function removeReferenceObjectProperties(payload: any) {
   return newPayload;
 }
 
-export function createEntityPayload(properties: any) {
+export function createLookupEntityPayload(properties: any) {
   const payload = {
     ...Object.entries(properties).reduce(
       (acc, [key, value]) => ({
@@ -98,73 +153,4 @@ export function createEntityPayload(properties: any) {
   };
   removeReferenceIdProperties(payload);
   return payload;
-}
-
-export function getEntity(entityName: string, id: number) {
-  return executeCrudMethod(entityName, "Get", id);
-}
-
-export function deleteEntity(entityName: string, id: number) {
-  return executeCrudMethod(entityName, "Delete", id);
-}
-
-export function updateEntity(entityName: string, args: any) {
-  switch (entityName) {
-    case "arrangement":
-    case "arrangementType":
-    case "branch":
-    case "client":
-    case "communicationType":
-    case "deliveryType":
-    case "responsible":
-    case "wrapper":
-    case "flower":
-      return executeCrudMethod(entityName, "Put", args);
-    case "flowerVariant":
-    case "wrapperVariant":
-      return executeCrudMethod(entityName, "Put", args);
-    default:
-      throw new Error("Invalid entity name");
-  }
-}
-
-export function createEntity(entityName: string, args: any) {
-  switch (entityName) {
-    case "arrangement":
-    case "arrangementType":
-    case "branch":
-    case "client":
-    case "communicationType":
-    case "deliveryType":
-    case "responsible":
-    case "wrapper":
-    case "flower":
-      return executeCrudMethod(entityName, "Post", args);
-    case "flowerVariant":
-    case "wrapperVariant":
-      return executeCrudMethod(entityName, "Post", args);
-    default:
-      throw new Error("Invalid entity name");
-  }
-}
-
-async function executeCrudMethod(
-  entityName: string,
-  methodName: MethodNames,
-  ...args: any[]
-) {
-  const client = selectClient(entityName);
-  if (typeof client[entityName + "_" + methodName] === "function") {
-    if (methodName === "Put") {
-      const { id } = args[0];
-      return await client[entityName + "_" + methodName](id, ...args);
-    }
-    return await client[entityName + "_" + methodName](...args);
-  } else {
-    throw new Error(
-      `Method ${
-        entityName + "_" + methodName
-      } does not exist on ${entityName}Client`
-    );
-  }
 }
