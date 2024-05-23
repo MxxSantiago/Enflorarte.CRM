5ac7691ced2a90ff2657d206d376656b380a9e86
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-05-22 20:04:42 -0700

    feat(PBI-ORD-US2-3): Add Get Orders By Month method

---
+++ src/Web/ClientApp/src/core/hooks/useOrderDateHooks.jsx
@@ -0,0 +1,28 @@
+﻿import {useQuery} from "@chakra-ui/react";
+import axios from "axios";
+
+const fetchOrdersByDate = async (date, interval) => {
+  let url;
+  switch (interval) {
+    case 'day':
+      url = `/api/Order/GetDayOrders?day=${date}`;
+      break;
+    case 'week':
+      url = `/api/Order/GetWeekOrders?week=${date}`;
+      break;
+    case 'month':
+      url = `/api/Order/GetMonthOrders?month=${date}`;
+      break;
+    default:
+      throw new Error(`Invalid interval: ${interval}`);
+  }
+
+  const { data } = await axios.get(url);
+  return data;
+};
+
+const useOrdersByDate = (date, interval) => {
+  return useQuery(['orders', date, interval], () => fetchOrdersByDate(date, interval));
+};
+
+export default useOrdersByDate;
