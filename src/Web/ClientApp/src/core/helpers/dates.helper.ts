e6f0cabbc2448a13d7929f4b57ff09e3e305e672
Max Santiago <mxxsant@hotmail.com>
2024-06-05 02:42:42 -0700

    feat(PBI-GEN-26): Document base helper functions

--- src/Web/ClientApp/src/core/helpers/dates.helper.ts
+++ src/Web/ClientApp/src/core/helpers/dates.helper.ts
@@ -1,3 +1,8 @@
+/**
+ * Converts a date to a local ISO string representation.
+ * @param {Date} date - The date to convert.
+ * @returns {string} The local ISO string representation of the date.
+ */
 export function toLocalISOString(date) {
   const offset = date.getTimezoneOffset();
   date = new Date(date.getTime() - offset * 60 * 1000);
