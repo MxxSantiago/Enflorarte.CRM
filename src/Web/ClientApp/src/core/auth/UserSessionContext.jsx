debd037c42a78d2f1808ccb2b0d1ec978c16ab21
Max Santiago <mxxsant@hotmail.com>
2024-06-05 02:34:40 -0700

    feat(PBI-ORD-24): Document the user session context code

--- src/Web/ClientApp/src/core/auth/UserSessionContext.jsx
+++ src/Web/ClientApp/src/core/auth/UserSessionContext.jsx
@@ -4,11 +4,23 @@ import { sessionStorageKey } from "../constants.ts";
 
 export const UserSessionContext = createContext();
 
+/**
+ * Provides the session context for the user.
+ * @component
+ * @param {Object} props - The component props.
+ * @param {ReactNode} props.children - The child components.
+ * @returns {JSX.Element} The session provider component.
+ */
 export const SessionProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [session, setSession] = useState(null);
 
   useEffect(() => {
+    /**
+     * Fetches the user session from the API and updates the session state.
+     * @async
+     * @function fetchSession
+     */
     const fetchSession = async () => {
       const user = await apiClient.getCurrentUser();
       localStorage.setItem(sessionStorageKey, JSON.stringify(user));
@@ -20,10 +32,21 @@ export const SessionProvider = ({ children }) => {
       .catch(() => setLoading(false));
   }, []);
 
+  /**
+   * Retrieves the roles of the current user.
+   * @function getUserRoles
+   * @returns {string[]} The roles of the current user.
+   */
   const getUserRoles = () => {
     return session ? session.roles : [];
   };
 
+  /**
+   * Checks if the current user has any of the specified roles.
+   * @function userHasRole
+   * @param {string[]} roles - The roles to check.
+   * @returns {boolean} True if the user has any of the specified roles, false otherwise.
+   */
   const userHasRole = (roles) => {
     if (roles == null) return true;
     const userRoles = getUserRoles();
